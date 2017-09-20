cdae6adbcf780bd6a4f524c6b3ca37e8e93fb1e4
hongwen <hongwen77@gmail.com>
2017-09-20 22:08:48 +0800

    Click event

--- demo-ng-uirouter/src/main/resources/static/about/about.component.js
+++ demo-ng-uirouter/src/main/resources/static/about/about.component.js
@@ -1,4 +1,57 @@
 angular.module('hello')
 .component('aboutComponent', {
-	templateUrl : '/about/about.template.html'
+    templateUrl : '/about/about.template.html'
 })
+.directive('myDialog', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            'close': '&onClose'
+        },
+        templateUrl: '/about/about.my-dialog.html'
+    };
+})
+.directive('entering', function() {
+    return function(scope, element) {
+        element.bind('mouseenter', function() {
+            scope.$apply("about.mouseIn()");
+        });
+    };
+
+})
+.directive('clicking', function() {
+    return function(scope, element) {
+        element.bind('click', function() {
+            scope.$apply("about.mouseClick()");
+        });
+    };
+
+});
+
+function AboutCtrl ($timeout) {
+    var self = this;
+    
+    self.name = 'Tobias';
+    self.message = '';
+    self.hideDialog = function(message) {
+        self.message = message;
+        self.dialogIsHidden = true;
+        $timeout(function() {
+            self.message = '';
+            self.dialogIsHidden = false;
+        }, 2000);
+    };
+    
+    self.mouseIn = function() {
+        alert('Mouse in!');
+    };
+    
+    self.mouseClick = function() {
+        alert('Mouse clicked!');
+    };
+}
+
+angular.module('hello')
+.controller('AboutCtrl', AboutCtrl)
+;
